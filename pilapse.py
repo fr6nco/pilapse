0779d43ff24792704fda4538e564ba3a865bcb81
Thomas Boros <tomas.boros92@gmail.com>
2014-11-12 21:23:33 +0100

    added threading

--- pilapse.py
+++ pilapse.py
@@ -1,11 +1,15 @@
 import vidsettings
-
-
+import pifacecad
+import picamera
+import inputter
 
 vsettings = vidsettings.vidSettings()
-
+cad = pifacecad.PiFaceCAD()
 vsettings.setInterval(5)
 vsettings.setNumphotos(10)
+
+inputter.importReader(cad, vsettings)
+
 
 print(vsettings.getExectime())
 
