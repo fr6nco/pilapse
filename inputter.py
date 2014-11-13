51ad9098e9e6faa7c2b1bcf9294c288b8ef6f4ab
Thomas Boros <tomas.boros92@gmail.com>
2014-11-13 23:08:52 +0100

    inputter seem to be ready

--- inputter.py
+++ inputter.py
@@ -1,38 +1,71 @@
 import pifacecad
+from time import sleep
+import threading
+
+
+class Barrier():
+    def __init__(self, n):
+        self.count = 0
+        self.n = n
+
+    def wait(self):
+        self.count +=1
+        while self.count < self.n:
+            sleep(0.001)
+
+
+threading.Barrier = Barrier
+
 
 class importReader():
     def __init__(self, cad=None):
         self.multiplier = 1
         self.value = 0
+        self.questionmsg = None
         if cad is None:
             self.cad = pifacecad.PiFaceCAD()
             self.cad.lcd.backlight_on()
             self.cad.lcd.clear()
         self.cad = cad
+        self.submitwait = None
 
-    def setMultiplier(self, multiplier):
+    def setmultiplier(self, multiplier):
         self.multiplier = multiplier
 
     def increaseby10(self):
-        print(self.multiplier)
-        print(self.value)
         self.value += (self.multiplier * 10)
-        print(self.value)
+        self.refreshdisplay()
 
     def decreaseby10(self):
         self.value -= (self.multiplier * 10)
         if self.value < 0:
             self.value = 0
+        self.refreshdisplay()
 
     def increaseby1(self):
         self.value +=(self.multiplier)
+        self.refreshdisplay()
 
     def decreaseby1(self):
         self.value -=(self.multiplier)
         if self.value < 0:
             self.value=0
+        self.refreshdisplay()
+
+    def submitbutton(self):
+        self.submitwait.wait()
+
+
+    def refreshdisplay(self):
+        self.cad.lcd.clear()
+        self.cad.lcd.set_cursor(0,0)
+        self.cad.lcd.write(self.questionmsg)
+        self.cad.lcd.set_cursor(0,1)
+        self.cad.lcd.write(self.value)
+
 
     def getInput(self, questionmsg):
+        self.questionmsg = questionmsg
         self.cad.lcd.clear()
         self.cad.lcd.backlight_on()
         self.cad.lcd.set_cursor(0,0)
@@ -40,10 +73,12 @@ class importReader():
         self.cad.lcd.set_cursor(0,1)
         self.cad.lcd.write("0")
         listener = pifacecad.SwitchEventListener(self.cad)
-        listener.register(7, pifacecad.IODIR_ON, self.increaseby1)
-        listener.register(6, pifacecad.IODIR_ON, self.decreaseby1)
+        listener.register(0, pifacecad.IODIR_ON, self.increaseby1)
+        listener.register(1, pifacecad.IODIR_ON, self.decreaseby1)
+        listener.register(2, pifacecad.IODIR_ON, self.submitbutton)
+        self.submitwait = Barrier(2);
         listener.activate
-        self.increaseby10()
+        self.submitwait.wait()
         listener.deactivate
         return self.value
 
--- pilapse.py
+++ pilapse.py
@@ -8,6 +8,11 @@ vsettings = vidsettings.vidSettings()
 inthread = inputter.importReader(cad)
 val = inthread.getInput("Interval ms")
 print(val)
+vsettings.setInterval(val)
+inthread.setmultiplier(10)
+val = inthread.getInput("# of photos")
+print(val)
+vsettings.setNumphotos(val)
 
 print(vsettings.getExectime())
 
