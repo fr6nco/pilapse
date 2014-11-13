a1d4ca78a3e34069a0bf45a665595f5ecbbd29e4
Thomas Boros <tomas.boros92@gmail.com>
2014-11-13 23:14:05 +0100

    fixing

--- inputter.py
+++ inputter.py
@@ -11,7 +11,7 @@ class Barrier():
     def wait(self):
         self.count +=1
         while self.count < self.n:
-            sleep(0.001)
+            sleep(0.0001)
 
 
 threading.Barrier = Barrier
@@ -20,7 +20,7 @@ threading.Barrier = Barrier
 class importReader():
     def __init__(self, cad=None):
         self.multiplier = 1
-        self.value = 0
+        self.value = 2
         self.questionmsg = None
         if cad is None:
             self.cad = pifacecad.PiFaceCAD()
@@ -33,23 +33,27 @@ class importReader():
         self.multiplier = multiplier
 
     def increaseby10(self):
+        print("Increasing by 10")
         self.value += (self.multiplier * 10)
         self.refreshdisplay()
 
     def decreaseby10(self):
+        print("Decreasing by 10")
         self.value -= (self.multiplier * 10)
         if self.value < 0:
             self.value = 0
         self.refreshdisplay()
 
     def increaseby1(self):
-        self.value +=(self.multiplier)
+        print("Increasing by 1")
+        self.value += self.multiplier
         self.refreshdisplay()
 
     def decreaseby1(self):
+        print("Decreasing by 1")
         self.value -=(self.multiplier)
         if self.value < 0:
-            self.value=0
+            self.value = 0
         self.refreshdisplay()
 
     def submitbutton(self):
@@ -71,11 +75,12 @@ class importReader():
         self.cad.lcd.set_cursor(0,0)
         self.cad.lcd.write(questionmsg)
         self.cad.lcd.set_cursor(0,1)
-        self.cad.lcd.write("0")
+        self.cad.lcd.write(self.value)
         listener = pifacecad.SwitchEventListener(self.cad)
         listener.register(0, pifacecad.IODIR_ON, self.increaseby1)
         listener.register(1, pifacecad.IODIR_ON, self.decreaseby1)
         listener.register(2, pifacecad.IODIR_ON, self.submitbutton)
+        print("Buttons were registered")
         self.submitwait = Barrier(2);
         listener.activate
         self.submitwait.wait()
